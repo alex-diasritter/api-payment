7acacd182e668a1608bf95e0273d4d602f5581ab
alex-diasritter <ritter.alex@hotmail.com>
2025-09-16 20:50:22 -0300

    feature: estrutura inicial

---
+++ src/main/java/com/alex/api_pix_qrcode/ApiPixQrcodeApplication.java
@@ -0,0 +1,11 @@
+package com.alex.api_pix_qrcode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPixQrcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPixQrcodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alex/api_pix_qrcode/ApiValuePixDTOQrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.api_pix_qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiValuePixDTOQrcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
