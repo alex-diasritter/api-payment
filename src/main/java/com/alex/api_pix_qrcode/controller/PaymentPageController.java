3dc9ec7222f636256b5ed2a3eeb65a534aafd158
alex-diasritter <ritter.alex@hotmail.com>
2025-09-16 21:53:04 -0300

    feature: endpoint @Controller para página html

---
+++ src/main/java/com/alex/api_pix_qrcode/controller/PaymentPageController.java
@@ -0,0 +1,14 @@
+package com.alex.api_pix_qrcode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaymentPageController {
+
+    @GetMapping("/pagamento")
+    public String pagamento() {
+        // retorna o nome do arquivo HTML sem extensão
+        return "pagamentos";  // src/main/resources/templates/pagamentos.html
+    }
+}
