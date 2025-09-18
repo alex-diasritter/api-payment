51decb2b77268e8b2358726d533bc201a90e315a
AlexRitter1 <ritter.alex@hotmail.com>
2025-09-18 12:51:36 -0300

    feature: logs e armazenamento de dados do cliente para envio de email

--- src/main/java/com/alex/api_pix_qrcode/controller/PixController.java
+++ src/main/java/com/alex/api_pix_qrcode/controller/PixController.java
@@ -1,6 +1,11 @@
 package com.alex.api_pix_qrcode.controller;
 import com.alex.api_pix_qrcode.dto.PixRequestDto;
+import com.alex.api_pix_qrcode.dto.PixResponseDto;
+import com.alex.api_pix_qrcode.service.EmailService;
 import com.alex.api_pix_qrcode.service.PixService;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,18 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/pix")
 public class PixController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PixController.class);
+
     @Autowired
     private PixService pixService;
 
-    @GetMapping("/pagamento")
-    public String pagamento() {
-        // retorna o nome do arquivo HTML sem extensão
-        return "pagamentos";
-    }
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping
-    public ResponseEntity pix(@RequestBody PixRequestDto client) {
-        var response = pixService.createClientAndPaymentPix(client);
+    public ResponseEntity<PixResponseDto> pix(@RequestBody PixRequestDto dto) {
+        emailService.storeUserInfo(dto);
+        logger.info("Enpoint PIX acionado. Gerando pix para: " + dto.email());
+        var response = pixService.createStaticQRCode();
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(response);
