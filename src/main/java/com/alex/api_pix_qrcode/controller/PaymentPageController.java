6ae29f4abe1d6b17596636ef0a2868e1f4e33a76
AlexRitter1 <ritter.alex@hotmail.com>
2025-09-18 12:51:02 -0300

    refact: deletando método errôneo

--- src/main/java/com/alex/api_pix_qrcode/controller/PaymentPageController.java
+++ src/main/java/com/alex/api_pix_qrcode/controller/PaymentPageController.java
@@ -6,13 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PaymentPageController {
 
-    @GetMapping("/pagamento")
-    public String pagamento() {
-        // retorna o nome do arquivo HTML sem extensão
-        return "pagamentos";  // src/main/resources/templates/pagamentos.html
-    }
-
-
     @GetMapping("/")
     public String home() {
         return "pagamentos"; // retorna pagamentos.html
